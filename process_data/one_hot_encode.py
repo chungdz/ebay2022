8bfa5c93b8589d5ca307e3de5bc9d509e5f84e6b
xiaoxin Hu <1033718596@qq.com>
2021-12-31 22:16:38 -0800

    restore record number

--- process_data/one_hot_encode.py
+++ process_data/one_hot_encode.py
@@ -25,7 +25,7 @@ c2 = pd.get_dummies(train_quiz.category_id, prefix='ci')
 c3 = pd.get_dummies(train_quiz.package_size, prefix='ps')
 c4 = pd.get_dummies(train_quiz.cross_city, prefix='cc')
 c5 = pd.get_dummies(train_quiz.cross_state, prefix='cs')
-train_quiz = pd.concat([train_quiz.drop(['record_number', 'shipment_method_id', 'category_id', 'package_size', 'cross_city', 'cross_state'], axis=1), 
+train_quiz = pd.concat([train_quiz.drop(['shipment_method_id', 'category_id', 'package_size', 'cross_city', 'cross_state'], axis=1), 
                c1, c2, c3, c4, c5], axis=1)
 
 train_quiz[:train_set.shape[0]].to_csv('data/parsed_train_cat.tsv', index=None, sep='\t')
